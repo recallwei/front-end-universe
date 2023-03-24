90526cc039fed0ad6aa0b163f7ae634e5edbf971
Bruce <recall4056@gmail.com>
2023-03-25 00:21:31 +0800

    chore: add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
   nitro: {
     preset: 'vercel-edge'
+  },
+  tailwindcss: {
+    viewer: true
   }
 })
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+})
+</script>
+
+<template>content</template>
