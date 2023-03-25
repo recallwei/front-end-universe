4eaf18e72e30ba2358f4b77082af6a0e40fb13a9
zhiblcx <1529088692@qq.com>
2023-03-25 16:04:55 +0800

    feat: some components

--- pages/Label.vue
+++ pages/Label.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="flex flex-wrap">
-    <router-link to="" class="bg-white p-1 pr-4 pl-4 rounded-3xl m-1 hover:shadow-xl duration-300" v-for="item in count"
-      v-bind:key="item">
+    <router-link
+      to=""
+      class="m-1 rounded-3xl bg-white p-1 pr-4 pl-4 duration-300 hover:shadow-xl"
+      v-for="item in count"
+      v-bind:key="item"
+    >
       <i>
-        <svg t="1679718548457" class="icon inline-block pr-1" viewBox="0 0 1024 1024" version="1.1"
-          xmlns="http://www.w3.org/2000/svg" p-id="2856" width="30" height="30">
-          <path d="M615.6 123.6h165.5L512 589.7 242.9 123.6H63.5L512 900.4l448.5-776.9z" fill="#41B883" p-id="2857">
-          </path>
-          <path d="M781.1 123.6H615.6L512 303 408.4 123.6H242.9L512 589.7z" fill="#34495E" p-id="2858"></path>
+        <svg
+          t="1679718548457"
+          class="icon inline-block pr-1"
+          viewBox="0 0 1024 1024"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          p-id="2856"
+          width="30"
+          height="30"
+        >
+          <path
+            d="M615.6 123.6h165.5L512 589.7 242.9 123.6H63.5L512 900.4l448.5-776.9z"
+            fill="#41B883"
+            p-id="2857"
+          ></path>
+          <path
+            d="M781.1 123.6H615.6L512 303 408.4 123.6H242.9L512 589.7z"
+            fill="#34495E"
+            p-id="2858"
+          ></path>
         </svg>
       </i>
       <span>Vue</span>
@@ -15,7 +34,7 @@
   </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
 import { ref } from 'vue'
 let count = ref(20)
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import Label from '~/pages/label.vue'
-import allSites from '~/pages/allSites.vue';
+import allSites from '~/pages/allSites.vue'
 import Collection from '~/pages/Collection.vue'
 import { ref } from 'vue'
 definePageMeta({
@@ -10,30 +10,30 @@ definePageMeta({
 
 let info = ref([
   {
-    'title': "技术博客",
-    "content": [
+    title: '技术博客',
+    content: [
       {
-        link: "https://dev.to/",
-        title: "dev.to",
-        message: "拥有百万开发者的技术社区，开发者分享交流，获取最新技术资讯，提高专业能力的好地方"
+        link: 'https://dev.to/',
+        title: 'dev.to',
+        message: '拥有百万开发者的技术社区，开发者分享交流，获取最新技术资讯，提高专业能力的好地方'
       },
       {
-        link: "https://juejin.cn/",
+        link: 'https://juejin.cn/',
         title: '掘金',
         message: '中文技术社区'
       }
     ]
   },
   {
-    'title': "技术团队",
-    "content": [
+    title: '技术团队',
+    content: [
       {
-        link: "https://dev.to/",
-        title: "dev.to",
-        message: "拥有百万开发者的技术社区，开发者分享交流，获取最新技术资讯，提高专业能力的好地方"
+        link: 'https://dev.to/',
+        title: 'dev.to',
+        message: '拥有百万开发者的技术社区，开发者分享交流，获取最新技术资讯，提高专业能力的好地方'
       },
       {
-        link: "https://juejin.cn/",
+        link: 'https://juejin.cn/',
         title: '掘金',
         message: '中文技术社区'
       }
@@ -46,6 +46,9 @@ let info = ref([
   <div>
     <Label></Label>
     <Collection class="mt-6 mb-10"></Collection>
-    <allSites v-for="item in info" :info="item"></allSites>
+    <allSites
+      v-for="item in info"
+      :info="item"
+    ></allSites>
   </div>
 </template>
